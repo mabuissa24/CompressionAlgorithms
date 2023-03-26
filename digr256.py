b5cc8101044400ebe66ed5fa43aaacf93159e5a6
dafg05 <danialefloresg@gmail.com>
2023-03-26 14:57:21 -0400

    Starter code

---
+++ digr256.py
@@ -0,0 +1,28 @@
+import bitstring as bs
+import digrCode as dc
+
+encodedMsg = bs.BitArray()
+
+def digr_encoder(text, code: list):
+    """
+    Implementation pending completion and testing of CodeDoubleMap class.
+    """
+
+    # codeWordLen = dc.bitsRequired(len(code) - 1)
+    # i = 0
+    # while True:
+    #     input = text[i:i+2]
+    #     encodeUnigram = True
+    #     for j in range(96, len(code)): # check if can encode a digram
+    #         if input == code[j]:
+    #             encodeUnigram = False
+    #             encodedMsg += "0b" + dc.getBin(j)
+    #             i += 2
+    #             break
+    #     if encodeUnigram:
+    #         for j in range(96):
+                
+                
+                
+            
+    
