5d39e82ebfdbfd41b60728c4392075a067dd806b
dafg05 <danialefloresg@gmail.com>
2023-04-04 15:53:06 -0400

    change

--- digr1024D.py
+++ digr1024D.py
@@ -2,16 +2,14 @@ from digrXXX import DigramCompression
 import sys
 
 if __name__ == "__main__":
-    if len (sys.argv) != 2 and len(sys.argv) != 3:
-        print("Usage: python digr256C.py <filenameNoExtension> [pathToFile]")
-        print("Default path: './encodings/'")
+    if len (sys.argv) != 2:
+        print("Usage: python digr1024D.py <filepathNoExtension>")
         sys.exit(1)
     
-    filenameWithPath = sys.argv[1]
-    if ".digr" in filenameWithPath:
-        print("Usage: python digr256C.py <filenameNoExtension> [pathToFile]")
-        print("Do not include code extension '.digr' in filename")
+    filepath = sys.argv[1]
+    if ".digr" in filepath:
+        print("Usage: python digr1024D.py <filepathNoExtension>")
+        print("Do not include code extension '.digr'")
         sys.exit(1)
-    path = sys.argv[2] if len(sys.argv) == 3 else "./encodings/"
     dc = DigramCompression(1024)
-    dc.decompressAndWrite(filenameWithPath, path)
+    dc.decompressAndWrite(filepath)
--- digr512C.py
+++ digr512C.py
@@ -2,13 +2,10 @@ from digrXXX import DigramCompression
 import sys
 
 if __name__ == "__main__":
-    if len (sys.argv) != 2 and len(sys.argv) != 3:
-        print("Usage: python digr256C.py <filename> [pathToFile]")
-        print("Default path: './'")
-        print("Example pathToFile: './books/")
+    if len (sys.argv) != 2:
+        print("Usage: python digr512C.py <filepath>")
         sys.exit(1)
     
-    filenameWithPath = sys.argv[1]
-    path = sys.argv[2] if len(sys.argv) == 3 else "./"
+    filepath = sys.argv[1]
     dc = DigramCompression(512)
-    dc.compressAndWrite(filenameWithPath, path)
+    dc.compressAndWrite(filepath)
